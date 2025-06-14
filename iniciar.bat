190a805fd6b312b719c91c1ac071240a1a483630
ElGamerProgramador5 <zaidtoledo03@gmail.com>
2025-06-14 01:51:14 -0600

    1.5.6 (activador)

--- iniciar.bat
+++ iniciar.bat
@@ -1,4 +1,4 @@
-@echo off
+@ec@echo off
 setlocal
 
 cd /d %~dp0
@@ -7,5 +7,14 @@ cd inventario
 :: Activar entorno virtual
 call env\Scripts\activate
 
-:: Lanzar servidor y luego abrir navegador cuando esté listo
-start "" cmd /c "python manage.py runserver && start http://127.0.0.1:8000/"
+:: Iniciar servidor en segundo plano con start /B
+start /B cmd /c "python manage.py runserver"
+
+:: Esperar unos segundos para asegurar que el servidor arranque
+timeout /t 2 >nul
+
+:: Abrir navegador
+start http://127.0.0.1:8000/
+
+exit
+
